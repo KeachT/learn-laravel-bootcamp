1b0f331bde099a73d1d5f040cbbba9a74d742901
KeachT <503.b03@gmail.com>
2024-09-23 01:50:43 +0900

    add delete action in drop down menu

--- resources/views/chirps/edit.blade.php
+++ resources/views/chirps/edit.blade.php
@@ -5,7 +5,9 @@
             @method('patch')
             <div>{{ $title }}</div>
             <textarea name="message"
-                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">{{ old('message', $chirp->message) }}</textarea>
+                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
+                {{ old('message', $chirp->message) }}
+            </textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
             <div class="mt-4 space-x-2">
                 <x-primary-button>{{ __('Save') }}</x-primary-button>
