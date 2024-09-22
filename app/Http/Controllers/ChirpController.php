9cbec1ba3b672a8d1abd064fe7b612e2f2e8bba6
KeachT <503.b03@gmail.com>
2024-09-22 15:52:30 +0900

    add route edit, update and actions

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class ChirpController extends Controller
@@ -13,7 +15,10 @@ class ChirpController extends Controller
      */
     public function index(): View
     {
-        return view('chirps.index');
+        return view(
+            'chirps.index',
+            ['chirps' => Chirp::with('user')->latest()->get()]
+        );
     }
 
     /**
@@ -49,17 +54,28 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Chirp $chirp)
+    public function edit(Chirp $chirp): View
     {
-        //
+        Gate::authorize('update', $chirp);
+
+        return view(
+            'chirps.edit',
+            ['chirp' => $chirp, 'title' => 'Edit Your Chirp']
+        );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate(['message' => 'required|string|max:255']);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
